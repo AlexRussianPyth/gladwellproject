9c41183e9bbe2f970c90a98236cd6e42f4cd3266
Alex Kleschov <alexkleschov@gmail.com>
2022-03-29 14:23:19 +0400

    change database from sqlite3 to postgresql

--- gladwellproject/settings.py
+++ gladwellproject/settings.py
@@ -64,12 +64,22 @@ WSGI_APPLICATION = 'gladwellproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "test1", # DB name
+        'USER': "moni", # in pgadmin
+        'PASSWORD': "sobaka123",
     }
 }
+
 
 
 # Password validation
